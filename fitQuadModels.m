d9bf2db04792a6d9dbc1e979275f374287251dd3
Seb James <seb.james@sheffield.ac.uk>
2015-03-18 16:37:46 +0000

    High level architecture of the function

--- fitQuadModels.m
+++ fitQuadModels.m
@@ -3,7 +3,15 @@ function [ modelCoeffs ] = fitQuadModels (X, y)
 % y is the output vector.
 % X is the matrix of input vectors.
 % returns model coefficients for a quadratic fit.
+% See also surf, meshgrid, linspace
 
+% Clean the data
+[XClean, yClean] = removeNaNs (X, y);
 
+% Fit the model
+modelCoeffs = fitModel (XClean, yClean);
+
+% Visualise results
+visResults (X, y, XClean, yClean, modelCoeffs);
 
 end
