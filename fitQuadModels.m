14ddc7991f1c178eea6ecbac4ad01056320a620c
Seb James <seb.james@sheffield.ac.uk>
2015-03-18 16:54:19 +0000

    Adds the fitModel implementation (untested)

--- fitQuadModels.m
+++ fitQuadModels.m
@@ -25,7 +25,19 @@ end % removeNaNs
 
 % private
 function modelCoeffs = fitModel (XClean, yClean)
-
+    nVars = size(XClean, 2); % 1 or 2 please.
+    switch nVars
+        case 1
+            A = [ones(size(XClean)), XClean, XClean.^2];
+        case 2
+            x1 = XClean(:,1); x2 = XClean(:,2);
+            A = [x1.^0, x1, x1.^2, x2, x2.^2, x1.*x2];
+        otherwise
+            % First arg is error identifier. It MUST contain a colon.
+            error ('fitQuadModel:WrongNumberOfVars', ...
+                   'X must have one or two columns');
+    end % switch
+    modelCoeffs = A\yClean;
 end % fitModel
 
 % private
