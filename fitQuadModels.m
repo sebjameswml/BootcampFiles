a9fe36684e83021324ec9f48bbe767079ab5562b
Seb James <seb.james@sheffield.ac.uk>
2015-03-18 16:29:43 +0000

    Initial empty function

---
+++ fitQuadModels.m
@@ -0,0 +1,9 @@
+function [ modelCoeffs ] = fitQuadModels (X, y)
+% fitQuadModels Do a quadratic fit to input data
+% y is the output vector.
+% X is the matrix of input vectors.
+% returns model coefficients for a quadratic fit.
+
+
+
+end
